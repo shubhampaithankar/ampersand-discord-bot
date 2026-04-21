4acb7b9eae7386baf32429e64d0894a6af4ca35d
shubhampaithankar <shubhampaithankar65@gmail.com>
2026-04-21 16:50:12 +0530

    feat: Move dotenv config to constants.ts

--- app.ts
+++ app.ts
@@ -1,9 +1,7 @@
-import { config } from "dotenv";
 import Client from "./src/client";
 
 (() => {
   try {
-    config();
     process.removeAllListeners("warning");
 
     const client = new Client();
